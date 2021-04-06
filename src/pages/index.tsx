c380d3f79396beb773f208559cf494ee804f0134
0xb0b1 <vcente82@gmail.com>
2021-04-06 17:40:04 -0300

    homepage content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,29 @@
 import Head from "next/head";
 
+import styles from "./home.module.scss";
+
 export default function Home() {
   return (
     <>
       <Head>
-        <title> Inicio | Ignews</title>
+        <title> Home | Ignews</title>
       </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, Welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.99 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
     </>
   );
 }
