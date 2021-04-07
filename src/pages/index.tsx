4c377fe1e49f90c7bf517ada610c02573cfb8336
0xb0b1 <vcente82@gmail.com>
2021-04-07 09:13:16 -0300

    subscription feature #testing

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
+import { GetServerSideProps } from "next";
+
 import Head from "next/head";
 import SubscribeButton from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
@@ -19,10 +29,10 @@ export default function Home() {
 
           <p>
             Get access to all the publications <br />
-            <span>for $9.99 month</span>
+            <span>for {product.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl Coding" />
@@ -30,3 +40,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve("price_1IdZjKJLsp0sEVUkylCZC0FP");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+  };
+};
