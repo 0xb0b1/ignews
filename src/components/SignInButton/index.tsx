d576eae09801f4356173538aa0ceb1dd1ba603cf
0xb0b1 <vcente82@gmail.com>
2021-04-29 10:16:32 -0300

    basic next authentication with github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -9,8 +9,6 @@ import styles from "./styles.module.scss";
 export function SignInButton() {
   const [session] = useSession();
 
-  console.log(session);
-
   return session ? (
     <button type="button" className={styles.signInButton}>
       <FaGithub color="#04d361" />
