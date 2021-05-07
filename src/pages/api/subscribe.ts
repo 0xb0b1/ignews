8548f4f76afd865f360a5e52f0e346c304635b07
0xb0b1 <vcente82@gmail.com>
2021-05-07 15:09:44 -0300

    ...

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,14 +1,38 @@
+import { query as q } from "faunadb";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/client";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+
+interface User {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+}
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "POST") {
     const session = await getSession({ req });
 
+    // const user = await fauna.query<User>(
+    //   q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.user.email)))
+    // );
+
     const stripeCustomer = await stripe.customers.create({
-      email: session.user.email,
+      // email: session.user.email,
+      // metadata
     });
+
+    // await fauna.query(
+    //   q.Update(q.Get(q.Index("user_by_email"), user.ref.id), {
+    //     data: {
+    //       stripe_customer_id: stripeCustomer.id,
+    //     },
+    //   })
+    // );
 
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: stripeCustomer.id,
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -4,7 +4,7 @@ import { version } from "../../package.json";
 export const stripe = new Stripe(process.env.STRIPE_API_KEY, {
   apiVersion: "2020-08-27",
   appInfo: {
-    name: "Ignews",
+    name: "ignews",
     version,
   },
 });
