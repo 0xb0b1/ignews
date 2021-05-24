a5f120d18ae3f328295a5ff56f047ab8129a7c0f
0xb0b1 <vcente82@gmail.com>
2021-05-24 14:30:53 -0300

    next auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,11 +1,33 @@
+import { signIn, signOut, useSession } from "next-auth/client";
+
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 import styles from "./styles.module.scss";
 
 export function SignInButton() {
+  const [session] = useSession();
+
+  if (session) {
+    return (
+      <button type="button" className={styles.signInButton}>
+        <FaGithub color="#04d361" />
+        {session.user.name}
+        <FiX
+          className={styles.closeIcon}
+          color="#CCC"
+          onClick={() => signOut()}
+        />
+      </button>
+    );
+  }
+
   return (
-    <button type="button" className={styles.signInButton}>
+    <button
+      onClick={() => signIn("github")}
+      type="button"
+      className={styles.signInButton}
+    >
       <FaGithub color="#eba417" />
       Sign in With GitHub
     </button>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { GetStaticProps } from "next";
+
 import Head from "next/head";
 import SubscribeButton from "../components/SubscribeButton";
+import { stripe } from "../services/stripe";
 
 import styles from "./home.module.scss";
 
@@ -26,9 +28,9 @@ export default function Home({ product }: HomeProps) {
           </h1>
           <p>
             Get access to all the publication <br />
-            <span>for $ 9.90 month</span>
+            <span>for {product.amount} month</span>
           </p>
-          <SubscribeButton />
+          <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -36,3 +38,22 @@ export default function Home({ product }: HomeProps) {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1ItyjPJLsp0sEVUkhosoARFj");
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+    }).format(price.unit_amount / 100),
+  };
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24horas
+  };
+};
