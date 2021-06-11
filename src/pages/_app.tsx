ac97c775256d541e7c731ab3325e95220af862ad
0xb0b1 <vcente82@gmail.com>
2021-06-11 14:37:26 -0300

    ...

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,13 +4,22 @@ import { AppProps } from "next/app";
 
 import { Header } from "../components/Header";
 
+import NextNprogress from "nextjs-progressbar";
+
 import "../styles/global.scss";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <NextAuthProvider session={pageProps.session}>
-      <Header />
-      <Component {...pageProps} />
+      <NextNprogress
+        color="#eba417"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      >
+        <Header />
+        <Component {...pageProps} />
+      </NextNprogress>
     </NextAuthProvider>
   );
 };
